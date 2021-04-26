320a482efed98cea17d445e15f19896de0a6763c
Rafael Casachi <rcasachi@gmail.com>
2021-04-26 12:25:41 -0300

    add: last steps

--- customize.sh
+++ customize.sh
@@ -5,4 +5,4 @@ pacman -Syu
 sudo pacman -S a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore gstreamer0.10-plugins 
 sudo pacman -S firefox aria2 wget git 
 sudo pacman -S p7zip p7zip-plugins unrar tar rsync
-
+sudo pacman -S firefox barrier git neovim
